fbaf23a73f0836121b4afa5d79b251c92d03b4b5
PANDEAD <sulthana05shaik20@gmail.com>
2025-11-27 10:17:49 +0530

    Hosted db on Aiven

--- app.py
+++ app.py
@@ -53,13 +53,16 @@ UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST", "localhost")
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST", "locallhost")
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT", "3306"))
+app.config['MYSQL_SSL_CA'] = os.getenv("MYSQL_SSL_CA")  
 
 if not app.config['MYSQL_USER'] or not app.config['MYSQL_PASSWORD'] or not app.config['MYSQL_DB']:
     raise RuntimeError("MySQL environment variables (MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB) must be set")
+
 
 mysql = MySQL(app)
 
